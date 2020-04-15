bc01d0473ee8310be8e9499e7c4d314ea067b49b
Cole Crain <54562876+crain28@users.noreply.github.com>
2020-04-15 13:20:19 -0400

    Add files via upload

---
+++ TBQuestGame.S3/Models/Character.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBQuestGame.Models
+{
+    /// <summary>
+    /// base class for all game characters
+    /// </summary>
+    public class Character : ObservableObject
+    {
+        #region ENUMS
+
+        public enum ClanType
+        {
+           Human,
+           HalfHuman,
+           Demon,
+           Angel
+        }
+
+        #endregion
+
+        #region FIELDS
+
+        protected int _id;
+        protected string _name;
+        protected int _locationId;
+        protected int _age;
+        protected ClanType _clan;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int LocationId
+        {
+            get { return _locationId; }
+            set { _locationId = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public ClanType Clan
+        {
+            get { return _clan ; }
+            set { _clan = value; }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public Character()
+        {
+
+        }
+
+        public Character(string name, ClanType clan, int locationId)
+        {
+            _name = name;
+            _clan = clan;
+            _locationId = locationId;
+        }
+
+        #endregion
+
+        #region METHODS
+
+        public virtual string DefaultGreeting()
+        {
+            return $"Hello, my name is {_name} and I am a {_clan}.";
+        }
+
+        #endregion
+    }
+}
